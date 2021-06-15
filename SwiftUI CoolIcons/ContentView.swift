c532309ac7b1febccc5fe7664142d9a109576d15
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-15 18:23:24 +0800

    Version 1.0

--- SwiftUI CoolIcons/ContentView.swift
+++ SwiftUI CoolIcons/ContentView.swift
@@ -5,12 +5,54 @@
 //  Created by Amben on 6/15/21.
 //
 
+import SwiftUIFontIcon
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var updateBg = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                VStack {
+                    HStack {
+                        Text("Material Icon (Assistant Icon)")
+                        Spacer()
+                        FontIcon.text(.materialIcon(code: .assistant),
+                                      fontsize: 80,
+                                      color: .blue)
+                    }// end of hstack
+                    .padding()
+                    
+                    HStack {
+                        Text("Material Icon (Sync Icon)")
+                        Spacer()
+                        FontIcon.text(.materialIcon(code: .sync),
+                                      fontsize: 80,
+                                      color: .yellow)
+                    }// end of hstack
+                    .padding()
+                    
+                    HStack {
+                        Text("Ionicon Button (Add Icon)")
+                        Spacer()
+                        FontIcon.button(.ionicon(code: .ios_add_circle),
+                                        action: {
+                                            self.updateBg.toggle()
+                                        },
+                                        padding: 0,
+                                        fontsize: 80,
+                                        color: .pink)
+                    }// end of hstack
+                    .padding()
+            
+                }// end of vstack
+            }// end of zstack
+            .ignoresSafeArea()
+            .background(updateBg ? Color.green : Color(.systemBackground))
+            .navigationTitle("Custom Icons")
+        }// end of navigation
     }
 }
 
